48f7cd4c829017c9226f8c34951e36d4d15e7523
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-02-27 22:54:57 +0200

    find random word

---
+++ script.js
@@ -0,0 +1,15 @@
+const categories = {
+    cities : ['Manchester', 'Milan', 'Madrid','Amsterdam','Prague'],
+    football_clubs: ['Everton','Liverpool','Swansea','Chelsea','Hull','Manchester city','Newcastle united'],
+    movies: ['Alien','Dirty harry','Gladiotor','Finding-nemo','Jaws']
+}
+
+const word_list = Object.keys(categories);
+const getRandom = (x) => x[Math.floor(Math.random() * x.length)];
+const random_category = getRandom(word_list);
+const random_word = getRandom(categories[random_category])
+console.log(random_word);
+ 
+
+
+
