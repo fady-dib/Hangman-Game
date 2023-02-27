795b0887a79dff6bba0a2d27f6251cb4c2d4157d
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-02-28 00:36:55 +0200

    declare variables

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const letters_div = document.getElementById('letters-div');
+const word_div = document.getElementById('word');
+const live_span = document.getElementById('lives');
+const reset_btn = document.getElementById('reset-btn');
+const notif = document.getElementById('notif');
+const notif_content = document.getElementById('notif-content');
+const notif_span = document.getElementById('notif-span');
+const play_again =  document.getElementById('play-again');
+
+let letters;
+let lives;
+
+
 const categories = {
     cities : ['Manchester', 'Milan', 'Madrid','Amsterdam','Prague'],
     football_clubs: ['Everton','Liverpool','Swansea','Chelsea','Hull','Manchester city','Newcastle united'],
@@ -9,6 +22,7 @@ const getRandom = (x) => x[Math.floor(Math.random() * x.length)];
 const random_category = getRandom(word_list);
 const random_word = getRandom(categories[random_category]);
 console.log(random_word);
+
 
  
 
